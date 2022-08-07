46cf387e27d0ce2222fc354f2bd5aa96c12374d0
Davinson Rincon <davinson.rincon@est.iudigital.edu.co>
2022-08-07 06:34:26 -0500

    first commit

---
+++ src/main/java/com/sofka/davs/socialMediaBack/SocialMediaBackApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.davs.socialMediaBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/davs/socialMediaBack/SocialMediaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.davs.socialMediaBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
